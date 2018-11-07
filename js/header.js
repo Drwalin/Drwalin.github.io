ec0d8ff4217732d2f0188a588a765fb8b01c0679
Drwalin <garekmarek@wp.pl>
2018-11-07 22:34:56 +0100

    Added header and footer generation in to javascript.

---
+++ js/header.js
@@ -0,0 +1,5 @@
+
+document.getElementById("headerGenerator").innerHTML = "<div id=\"container\">			<div class=\"mainReferencesLeft\">				<a href=\"https://DrwalinPCF.github.io\">Main Page</a>			</div>						<div class=\"mainReferencesRight\">				<a href=\"https://DrwalinPCF.github.io/posts.html\">Other posts</a>				<a href=\"https://DrwalinPCF.github.io/postsList.html\">List of post series</a>				<a href=\"https://DrwalinPCF.github.io/contactme.html\">Contact me</a>			</div>					</div>";
+
+
+document.getElementById("footerGenerator").innerHTML = "		<div id=\"container\">					<div class=\"footerLeft\">				<a href=\"https://facebook.com\">Facebook fanpage</a>				<a href=\"https://twitter.com\">Twittersomething</a>			</div>						<div class=\"footerRight\">				<a>EMail: <i><u><font color=\"#2020d0\">########@gmail.com</font></u></i><a>			</div>					</div>";
